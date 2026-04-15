6703f45063f4e1e04c0b6e81906c95cd2ac8b5b9
Mayyygomes29 <maya.gomessilva29@gmail.com>
2026-04-15 12:59:18 -0300

    config os no extract

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
+import os
 # =========================
 # EXTRACT
 # =========================
 def extract():
-    return pd.read_csv('data/dataR2.csv')
+    path = os.path.join("data", "dataR2.csv")
+    return pd.read_csv(path)
     
